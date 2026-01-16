2a4e9c5819a196cbdfe802de800075f011e36fb0
AlanIslasZd <160071378+AlanIslasZd@users.noreply.github.com>
2026-01-15 19:05:53 -0600

    Adjust partitioning in SQL query for duplicates

--- 01_Discovery/01_Grain_of_the_df/possible_duplicates_even_when_combining_supplier_and_invoices.sql
+++ 01_Discovery/01_Grain_of_the_df/possible_duplicates_even_when_combining_supplier_and_invoices.sql
@@ -3,7 +3,7 @@ SELECT
         *,
         -- We partition by the 5 values you identified to catch exact duplicates
         ROW_NUMBER() OVER (
-            PARTITION BY INVOICE__, SUPPLIER, TOTAL_USD-- PAYMENT_DATE, REQUESTER, TOTAL_USD, INVOICE_DATE
+            PARTITION BY INVOICE__, SUPPLIER-- PAYMENT_DATE, REQUESTER, TOTAL_USD, INVOICE_DATE
             ORDER BY (SELECT NULL) -- Keep any of the identical rows
         ) as rn
     FROM _sandbox_working_capital.working_capital.wc_ap_zdp
