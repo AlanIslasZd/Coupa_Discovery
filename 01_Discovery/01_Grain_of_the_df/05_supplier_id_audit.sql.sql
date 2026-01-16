84dbfe4d19dd49d0d77c2e0870c320834ca53075
AlanIslasZd <160071378+AlanIslasZd@users.noreply.github.com>
2026-01-15 19:05:10 -0600

    Refactor SQL query for supplier ID audit
    Modified the SQL query to adjust the partitioning criteria and added a filter for payment dates beyond 2024-12-31.

--- 01_Discovery/01_Grain_of_the_df/05_supplier_id_audit.sql.sql
+++ 01_Discovery/01_Grain_of_the_df/05_supplier_id_audit.sql.sql
@@ -3,13 +3,17 @@ SELECT
         *,
         -- We partition by the 5 values you identified to catch exact duplicates
         ROW_NUMBER() OVER (
-            PARTITION BY INVOICE__, SUPPLIER, PAYMENT_DATE, REQUESTER, TOTAL_USD, INVOICE_DATE
+            PARTITION BY INVOICE__, SUPPLIER, TOTAL_USD-- PAYMENT_DATE, REQUESTER, TOTAL_USD, INVOICE_DATE
             ORDER BY (SELECT NULL) -- Keep any of the identical rows
         ) as rn
     FROM _sandbox_working_capital.working_capital.wc_ap_zdp
+    where 1=1 
+    and payment_date > '2024-12-31'
 )
 SELECT * 
-FROM t1 
-where RN > 1
+FROM _sandbox_working_capital.working_capital.wc_ap_zdp 
+where INVOICE__ IN (
+select distinct INVOICE__ from t1 where rn >1
+) 
 ORDER BY 
 INVOICE__, SUPPLIER, PAYMENT_DATE, REQUESTER, TOTAL_USD
